5f6b3c21f3d70a2cd6343870307d589475f1b22a
Sanekli <153511859+Sanekli@users.noreply.github.com>
2024-06-21 15:05:02 +0100

    Delete forclient directory

--- forclient/src/Redux/Consts/action-type.js
+++
@@ -1,8 +0,0 @@
-export const SIGNUP = "SIGNUP";
-export const SIGNIN = "SIGNIN";
-export const LOGOUT = "LOGOUT";
-export const CURRENT = "CURRENT";
-export const PRODUCT = "PRODUCT";
-export const GET_PRODUCT = "GET_PRODUCT";
-export const RESERVATION = "RESERVATION";
-export const GET_RESERVATION = "GET_RESERVATION";
--- forclient/src/Redux/Reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import Reducers from "./reducers";
-
-const rootReducer = combineReducers({ Reducers });
-export default rootReducer;
